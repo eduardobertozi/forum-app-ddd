c6c0caf14a1aabad87a0250010a4f836c3583630
eduardobertozi <webdu15@gmail.com>
2025-02-05 12:47:26 -0300

    feat(use-cases): implements create static method to create answer

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityId } from '@/core/entities/value-objects/unique-entity-id'
 import { Answer } from '../entities/answer'
 import { AnswersRepositoru } from '../repositories/answers.repository'
 
@@ -11,10 +12,10 @@ export class AnswerQuestionUseCase {
   constructor(private answersRepository: AnswersRepositoru) {}
 
   async execute({ questionId, instructorId, content }: AnswerQuestionInput) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEntityId(instructorId),
+      questionId: new UniqueEntityId(questionId),
     })
 
     await this.answersRepository.create(answer)
