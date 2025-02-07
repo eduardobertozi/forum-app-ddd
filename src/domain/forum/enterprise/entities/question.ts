9d0d52a0247acbcdeef5f2c79f3907cb0036e7a3
eduardobertozi <webdu15@gmail.com>
2025-02-07 15:43:43 -0300

    refactor: export QuestionProps

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -4,7 +4,7 @@ import { Entity } from '@/core/entities/entity'
 import { Optional } from '@/core/types/optional'
 import dayjs from '@/lib/dayjs'
 
-interface QuestionProps {
+export interface QuestionProps {
   authorId: UniqueEntityId
   bestAnswerId?: UniqueEntityId
   title: string
