64887a09b5c9cab6433eca7455b1e64d8a8d1cf1
eduardobertozi <webdu15@gmail.com>
2025-02-07 15:45:34 -0300

    feat: create GetQuestionBySlugUseCase and unit test

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,26 @@
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionsRepositoru } from '@/domain/forum/application/repositories/questions.repository'
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string
+}
+
+interface GetQuestionBySlugUseCaseResponse {
+  question: Question
+}
+
+export class GetQuestionBySlugUseCase {
+  constructor(private questionsRepository: QuestionsRepositoru) {}
+
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionsRepository.findBySlug(slug)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    return { question }
+  }
+}
