deddac317e34a27319f4db05b44e3800c50fa70d
eduardobertozi <webdu15@gmail.com>
2025-02-07 16:00:15 -0300

    feat: enhance makeQuestion factory to use faker for dynamic content

--- test/factories/make-questions.ts
+++ test/factories/make-questions.ts
@@ -1,15 +1,23 @@
-import { QuestionProps } from '@/domain/forum/enterprise/entities/question'
+import {
+  Question,
+  QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { UniqueEntityId } from '@/core/entities/value-objects/unique-entity-id'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    authorId: new UniqueEntityId(),
-    content: 'A exemple question',
-    ...override,
-  })
-
-  return question
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  return Question.create(
+    {
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityId(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 }
